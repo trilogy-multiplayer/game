f48ef886d20f588d497f7cac476fa28a2d8ab544
Xalo Divo <70486444+Dschahannam@users.noreply.github.com>
2023-09-02 04:06:47 +0200

    feature(discord-rpc): push

---
+++ trilogy-client/networking/features/feature_discord-rpc.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef TRILOGY_NETWORKING_FEATURE_DISCORD_RPC_H
+#define TRILOGY_NETWORKING_FEATURE_DISCORD_RPC_H
+
+#include <common.hpp>
+
+#include <memory/memory.hpp>
+#include <random>
+
+namespace networking::features {
+	class c_feature_discord_rpc : public c_singleton<c_feature_discord_rpc> {
+	private:
+		
+	};
+}
+
+#endif
