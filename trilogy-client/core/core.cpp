8a373c0525d7f97f5913c6122b9208f1b31d0579
ffff <fffff>
2023-09-10 04:13:55 +0200

    core

--- trilogy-client/core/core.cpp
+++ trilogy-client/core/core.cpp
@@ -2,21 +2,8 @@
 #include <filesystem>
 #include <iostream>
 
-std::string c_core::get_computer_name() {
-	const int buffer_size = MAX_COMPUTERNAME_LENGTH + 1;
 
-	char buffer[buffer_size];
-	DWORD lpnSize = buffer_size;
-
-	if (GetComputerNameA(buffer, &lpnSize) == FALSE)
-		throw std::runtime_error("Something went wrong.");
-
-	return std::string{ buffer };
-}
-
-std::string c_core::get_trilogy_base_path()
-{
-	printf("Path: %s\n", std::filesystem::current_path().string().c_str());
+std::string c_core::get_trilogy_base_path() {
 	return std::filesystem::current_path().string().c_str();
 }
 
--- trilogy-client/core/core.hpp
+++ trilogy-client/core/core.hpp
@@ -6,13 +6,8 @@
 #include <common.hpp>
 
 class c_core : public c_singleton<c_core> {
-private:
-	std::string m_path = std::string("C:\\Games\\Grand Theft Auto San Andreas The Definitive Edition\\Gameface\\Binaries\\Win64\\");
-
 public:
-	std::string get_computer_name();
 	std::string get_trilogy_base_path();
-
 	std::string make_trilogy_path(std::string trilogy_path);
 };
 
